b97d41a9f6a0acee7a0ed3879da73f2e224dec93
Elisa Yan <elisa.yan@studio.unibo.it>
2026-04-27 13:55:09 +0200

    feat: improve connection management

--- haochi-cook-and-pass/client/src/main/connection.py
+++ haochi-cook-and-pass/client/src/main/connection.py
@@ -5,36 +5,51 @@ from queue import Queue
 
 msg_queue = Queue()   # Messaggi in entrata (dal Server al Client)
 send_queue = Queue()  # Messaggi in uscita (dal Client al Server)
+running = True
 
 async def websocket_client():
+    global running
     uri = "ws://localhost:8765"
+
+    websocket = None
+
     try:
-        async with websockets.connect(uri) as websocket:
-            print("Connesso al server!")
+        websocket = await websockets.connect(uri)
+        print("Connesso al server!")
 
-            # Gestore per l'INVIO dei messaggi
-            async def send_handler():
-                while True:
-                    if not send_queue.empty():
-                        msg = send_queue.get()
-                        await websocket.send(msg)
-                    # Evita di consumare troppa CPU
-                    await asyncio.sleep(0.1) 
+        async def send_handler():
+            while running:
+                if not send_queue.empty():
+                    msg = send_queue.get()
+                    await websocket.send(msg)
+                await asyncio.sleep(0.1)
 
-            # Gestore per la RICEZIONE dei messaggi
-            async def recv_handler():
-                try:
-                    async for message in websocket:
+        async def recv_handler():
+            try:
+                while running:
+                    try:
+                        message = await asyncio.wait_for(websocket.recv(), timeout=0.2)
                         msg_queue.put(message)
-                except websockets.exceptions.ConnectionClosed:
-                    print("Connessione chiusa dal server")
+                    except asyncio.TimeoutError:
+                        continue
+            except websockets.exceptions.ConnectionClosed:
+                print("Connessione chiusa")
 
-            # Esegue entrambi i compiti contemporaneamente
-            await asyncio.gather(send_handler(), recv_handler())
-            
+        await asyncio.gather(send_handler(), recv_handler())
+
     except Exception as e:
         print(f"Errore di rete: {e}")
+
+    finally:
+        if websocket:
+            await websocket.close()
+        print("websocket chiuso")
 
 def start_network():
     """Funzione punto di ingresso per il Thread."""
     asyncio.run(websocket_client())
+
+def shutdown():
+    global running
+    print("chiusura rete...")
+    running = False
